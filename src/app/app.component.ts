60602fbc9a3c92f2f1adb0625a4c1ee6adf50ff2
tumit <tumit@live.com>
2019-02-18 09:49:58 +0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'odds-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'odds-scss-demo';
+}
