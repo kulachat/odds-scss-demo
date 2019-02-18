f75495c3e916938ae2022f41f2918af331ff3364
tumit <tumit@live.com>
2019-02-18 10:34:45 +0700

    feat: add simple-card.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { SimpleCardComponent } from "./components/simple-card/simple-card.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, SimpleCardComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/simple-card/simple-card.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "odds-simple-card",
+  templateUrl: "./simple-card.component.html",
+  styleUrls: ["./simple-card.component.scss"]
+})
+export class SimpleCardComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
